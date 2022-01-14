6514f3051d59977e52084f68c9162afe6c5f98ed
FCWorkSpace <c.igor@list.ru>
2022-01-14 23:54:20 +0700

    add START & executeMessage methods, constants for Core

--- src/main/java/sibirbear/core/CoreBot.java
+++ src/main/java/sibirbear/core/CoreBot.java
@@ -1,8 +1,17 @@
 package sibirbear.core;
 
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
 import sibirbear.config.Config;
+import sibirbear.service.SendMessageBotService;
+
+import java.io.Serializable;
+
+import static sibirbear.core.CoreConstants.START;
 
 
 /*
@@ -13,11 +22,24 @@ public class CoreBot extends TelegramLongPollingBot {
 
     private final String token = Config.getConfigTelegramSettings().getToken();
     private final String botName = Config.getConfigTelegramSettings().getBotName();
+    private SendMessageBotService sendMessageBotService = new SendMessageBotService();
 
     @Override
     public void onUpdateReceived(Update update) {
         if (update.hasMessage() && update.getMessage().hasText()) {
-            //дописать
+            switch (update.getMessage().getText()) {
+                case START:
+                    executeMessage(sendMessageBotService.startMessage(update));
+                    break;
+            }
+        }
+    }
+
+    private <T extends BotApiMethod> void executeMessage(SendMessage sendMessage) {
+        try {
+            execute(sendMessage);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
         }
     }
 
---
+++ src/main/java/sibirbear/core/CoreConstants.java
@@ -0,0 +1,5 @@
+package sibirbear.core;
+
+public class CoreConstants {
+    public static final String START = "/start";
+}
