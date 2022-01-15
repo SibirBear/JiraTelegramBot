73ff9557f7680a5697ac0b4af4e3f0f788644b9a
FCWorkSpace <c.igor@list.ru>
2022-01-16 00:14:34 +0700

    add Steps, update User (for using steps) & CoreBot

--- src/main/java/sibirbear/core/CoreBot.java
+++ src/main/java/sibirbear/core/CoreBot.java
@@ -1,16 +1,12 @@
 package sibirbear.core;
 
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
-import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import sibirbear.config.Config;
 import sibirbear.service.SendMessageBotService;
-import sibirbear.service.SendMessageConstantText;
-
-import java.io.Serializable;
 
 import static sibirbear.core.CoreConstants.START;
 
@@ -23,22 +19,20 @@ public class CoreBot extends TelegramLongPollingBot {
 
     private final String token = Config.getConfigTelegramSettings().getToken();
     private final String botName = Config.getConfigTelegramSettings().getBotName();
-    private SendMessageBotService sendMessageBotService = new SendMessageBotService();
+    private final SendMessageBotService sendMessageBotService = new SendMessageBotService();
 
     @Override
     public void onUpdateReceived(Update update) {
         if (update.hasMessage() && update.getMessage().hasText()) {
-            switch (update.getMessage().getText()) {
-                case START:
-                    executeMessage(sendMessageBotService.startMessage(update));
-                    executeMessage(sendMessageBotService.authtorizationMessage(update));
+            if (START.equals(update.getMessage().getText())) {
+                executeMessage(sendMessageBotService.startMessage(update));
+                executeMessage(sendMessageBotService.authtorizationMessage(update));
 
-                    break;
             }
         }
     }
 
-    private <T extends BotApiMethod> void executeMessage(SendMessage sendMessage) {
+    private void executeMessage(SendMessage sendMessage) { //private <T extends BotApiMethod>
         try {
             execute(sendMessage);
         } catch (TelegramApiException e) {
--- src/main/java/sibirbear/model/User.java
+++ src/main/java/sibirbear/model/User.java
@@ -3,18 +3,21 @@ package sibirbear.model;
 import java.time.LocalDate;
 
 /*
- * Модель для хранения пользователя Jira с датой авторизации, чтобы в дальнейшем очищать
- * таблицу если дата авторизации истекла (в соответсвии с установленными правилами)
+ * Модель для хранения пользователя Jira с датой авторизации и номером следующего не завершенного шага.
+ * На основавнии сохраненного шага при получения сообщения выполняется соответствующий метод.
+ * На основании даты авторизации данный Пользователь будет удален из таблицы для повторной авторизации.
  */
 
 public class User {
 
     private String userName;
     private LocalDate date;
+    private Steps step;
 
-    public User(final String userName, final LocalDate date) {
+    public User(final String userName, final LocalDate date, final Steps step) {
         this.userName = userName;
         this.date = date;
+        this.step = step;
     }
 
     public String getUserName() {
@@ -23,6 +26,10 @@ public class User {
 
     public LocalDate getDate() {
         return date;
+    }
+
+    public Steps getStep() {
+        return step;
     }
 
 }
