4bf46b7810336444330a381ddbde623ee2e39496
FCWorkSpace <c.igor@list.ru>
2022-03-11 18:47:13 +0700

    add JiraConstants

---
+++ src/main/java/sibirbear/jiraAPI/JiraConstants.java
@@ -0,0 +1,18 @@
+package sibirbear.jiraAPI;
+
+public class JiraConstants {
+
+//Projects (PROJECT_)
+    public static final String PROJECT_FRANCH = "18990";
+    public static final String PROJECT_SUPPORT = "12390";
+
+//Issue types (ISSUE_TYPE_)
+    public static final String ISSUE_TYPE_REGULAR = "29";
+    public static final String ISSUE_TYPE_CREATE_GOODS = "11201";
+    public static final String ISSUE_TYPE_REPAIR = "11300";
+
+//Custom fields (FIELD_CUSTOM_)
+    public static final String FIELD_CUSTOM_CONTACT = "customfield_10420";  // соответствует полю "Контактное лицо"
+    public static final String FIELD_CUSTOM_DIVISION = "customfield_10231"; // соответствует полю "Подразделение"
+
+}
