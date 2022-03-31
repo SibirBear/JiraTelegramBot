e203ce7d239fc40a83978c6859133fadd29a3680
FCWorkSpace <c.igor@list.ru>
2022-03-31 15:24:04 +0700

    create JiraIssueURL for list issues

---
+++ src/main/java/sibirbear/jiraAPI/exceptions/JiraIssueURL.java
@@ -0,0 +1,27 @@
+package sibirbear.jiraAPI.exceptions;
+
+import sibirbear.jiraAPI.JiraConstants;
+
+public class JiraIssueURL {
+
+    private final String url;
+    private final String description;
+
+    /**
+     * Класс для хранения ссылки на заявку в Jira и 30знаков из ее описания для вывода списка открытых заявок
+     * @param key ключ заявки вида ПРОЕКТ-НОМЕР ЗАЯВКИ (TEST-1234)
+     * @param description описание заявки
+     */
+    public JiraIssueURL(final String key, final String description) {
+        this.url = JiraConstants.JIRA_ISSUE_URL_BROWSE + key;
+        this.description = description.substring(0, JiraConstants.JIRA_ISSUE_URL_DESCRIPTION_LENGTH);
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
