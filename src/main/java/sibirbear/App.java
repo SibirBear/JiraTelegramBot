e392c69f69d947b449af0a9f79960ab153329e17
FCWorkSpace <c.igor@list.ru>
2022-04-27 18:40:51 +0700

    update App with refactored CheckDivision

--- src/main/java/sibirbear/App.java
+++ src/main/java/sibirbear/App.java
@@ -4,11 +4,14 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.meta.generics.BotSession;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 import sibirbear.config.Config;
 import sibirbear.core.CoreBot;
 
 import java.util.Arrays;
+
+import static sibirbear.service.CheckDivision.setDivisionsFromDB;
 
 /*
  * Основной класс запуска
@@ -17,14 +20,30 @@ public class App {
 
     private static final Logger log = LogManager.getLogger(App.class);
 
+    // TODO: на старте проверять папку TempImage (если создана) и удалять оттуда файлы
+    //
+    // TODO: сделать класс для удаления мусора (папка TempImage, User, Order) который через определенный
+    //  интервал удаляет не актуальные данные или при завершении программы. Не актуальные данные:
+    //  1. Юзер авторизован более установленного времени - удаление юзера, его данных по заявке
+    //  2. Данные по заявке не используются более 12 часов - удаляем данные
+    //  3. Удаляем файлы в TempImage если на них нет ссылки в Orders
+
     public static void main(String[] args) {
         Config.init();
         log.info("[" + Config.APP_NAME + "] - Configuration properties are initialized.");
 
+       // TODO: сделать выполнение задачи на старте и по расписанию раз в день
+        setDivisionsFromDB();
+
         TelegramBotsApi botsApi;
         try {
             botsApi = new TelegramBotsApi(DefaultBotSession.class);
-            botsApi.registerBot(new CoreBot());
+            BotSession botSession = botsApi.registerBot(new CoreBot());
+
+            /* //При завершении
+            botSession.stop();
+            */
+
             log.info("[" + Config.APP_NAME + "] - Bot starting...");
 
             System.out.println("Bot starting...");
