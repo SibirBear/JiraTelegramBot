b61ae5a728b8f18d62009e1f8d9a60a02296258e
FCWorkSpace <c.igor@list.ru>
2022-04-27 18:40:04 +0700

    refactor CheckDivision

--- src/main/java/sibirbear/service/CheckDivision.java
+++ src/main/java/sibirbear/service/CheckDivision.java
@@ -11,38 +11,29 @@ import java.util.Map;
 
 public class CheckDivision {
 
+    private static final Map<String, String> storeDivision = new HashMap<>();
     private static final String SQL_QUERY = "select "
             + "store.store, store.store_name from rmsprd.store "
             + "join rmsprd.wf_customer on store.wf_customer_id=wf_customer.wf_customer_id "
             + "where wf_customer.wf_customer_group_id=2 "
             + "and store.store_close_date is null";
 
-    // TODO
-    //  1. По сути не нужно записывать весь ответ на запрос и потом проверять есть ли там нужный ключ или нет
-    //  Можно сразу проверять наличие ключа в запросе если проверять в цикле while
-    //  2. Или при инициализации приложения делать запрос и сохранять весь результат в Мапу, и проверять раз в сутки
-    //  или даже раз в 3е суток, чаще нет смысла, данные после создания раньше не понадобятся
-
-    public boolean isDivisionReal(String division) {
-        return getMapOfDivisionsFromDB().containsKey(division);
+    public static boolean isDivisionReal(String division) {
+        return storeDivision.containsKey(division);
     }
 
-    private Map<String, String> getMapOfDivisionsFromDB() {
+    public static void setDivisionsFromDB() {
         ConnectOracleDB db = new ConnectOracleDB();
-
-        Map<String, String> mapResponse = new HashMap<>();
 
         try (Connection con = db.initDB().getConnection();
              Statement st = con.createStatement();
              ResultSet rs = st.executeQuery(SQL_QUERY)) {
             while (rs.next()) {
-                mapResponse.put(rs.getString(1), rs.getString(2));
+                storeDivision.put(rs.getString(1), rs.getString(2));
             }
         } catch (SQLException e) {
             e.printStackTrace();
         }
-
-        return mapResponse;
 
     }
 
