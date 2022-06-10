cd6aa754cc87da44d6a1d91bef2f7f85ccaf1bf1
FCWorkSpace <c.igor@list.ru>
2022-06-10 12:27:59 +0700

    refactor: new check in CreateJiraIssue

--- src/main/java/info/fermercentr/jiraAPI/CreateJiraIssue.java
+++ src/main/java/info/fermercentr/jiraAPI/CreateJiraIssue.java
@@ -8,7 +8,7 @@ import info.fermercentr.jiraAPI.issue.IssueBuilder;
 public class CreateJiraIssue {
 
     public static String createJiraIssue(final JiraAPI api, final Issue issue) throws JiraApiException {
-        if (api == null) throw new JiraApiException("Cannot create Jira issue, parameter api is null.");
+        if (api == null || issue == null) throw new JiraApiException("Cannot create Jira issue, parameter api or issue is null.");
 
         String query = new IssueBuilder(issue).build();
         String key = api.httpCreateIssue(query);
